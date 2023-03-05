c073b428708e4984bb552497e2abab71fb49837c
Pmspsn <98259769+Pmspsn@users.noreply.github.com>
2023-03-05 14:39:55 +0700

    website demo cnpm

---
+++ ReadMe.md.txt
@@ -0,0 +1,2 @@
+add a db shop_db to phpadmin
+use xampp to open the website in localhost/mywebsite/
---
+++ components/footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <!-- <a href="about.php"> <i class="fas fa-angle-right"></i> About</a> -->
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Cửa hàng</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Liên hệ</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <!-- <a href="user_login.php"> <i class="fas fa-angle-right"></i> Đăng nhập</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Đăng kí</a> -->
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Giỏ hàng</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Đơn hàng</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us</h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> Số điện thoại 1</a>
+         <a href="tel:11122233333"><i class="fas fa-phone"></i> Số điện thoại 2</a>
+         <a href="mailto:shaikh@gmail.com"><i class="fas fa-envelope"></i> blabla@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Vị trí </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <!-- <a href="#"><i class="fab fa-linkedin"></i>linkedin</a> -->
+      </div>
+
+   </section>
+
+ 
+</footer>
